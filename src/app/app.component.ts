1f06f39fa7a349b82ecf3b47d8a39fedbe4f10bf
Xolile Dlamini <xolile@lintam.com>
2024-08-12 12:19:51 +0200

    Added The results table and the Calcutaions functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { UserInputComponent } from "./components/user-input/user-input.component";
+import type { InvestmentInput } from './investment-input-model';
+import { InvestmentResultsComponent } from "./components/investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'investment-calculator';
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear:number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+    }[];
+
+  onCalculateInvestmentResults(data:InvestmentInput) {
+    const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+        const year = i + 1;
+        const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+        investmentValue += interestEarnedInYear + annualInvestment;
+        const totalInterest =
+            investmentValue - annualInvestment * year - initialInvestment;
+        annualData.push({
+            year: year,
+            interest: interestEarnedInYear,
+            valueEndOfYear: investmentValue,
+            annualInvestment: annualInvestment,
+            totalInterest: totalInterest,
+            totalAmountInvested: initialInvestment + annualInvestment * year,
+        });
+    }
+
+    this.resultsData = annualData;
+    
+}
 }
---
+++ src/app/components/user-input/user-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../../investment-input-model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+ this.calculate.emit(
+  {
+    initialInvestment: +this.enteredInitialInvestment,
+    annualInvestment:  +this.enteredAnnualInvestment,
+    expectedReturn: + this.enteredExpectedReturn,
+    duration: +this.enteredDuration
+}
+ );
+  }
+}
